5a1315f17a9221effc01871a330574454bebb119
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-02-26 16:37:46 +0530

    Final CORS stable

--- loanmate-backend/server.js
+++ loanmate-backend/server.js
@@ -8,21 +8,19 @@ connectDB();
 
 const app = express();
 
+/* SIMPLE CORS */
 app.use(cors());
-app.options("*", cors());
 
 app.use(express.json());
 
-
+/* ROUTES */
 app.use("/api/auth", require("./routes/Authroutes"));
 app.use("/api/persons", require("./routes/Personroutes"));
 app.use("/api/loans", require("./routes/Loanroutes"));
 
-
 app.get("/", (req, res) => {
   res.send("LoanMate Backend is running 🚀");
 });
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
