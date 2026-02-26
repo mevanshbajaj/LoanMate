44b33de5c5166779ae5d84cbe6a095b65bd80c75
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-02-26 20:07:17 +0530

    Cors updateddd

--- loanmate-frontend/src/pages/login.jsx
+++ loanmate-frontend/src/pages/login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { API } from "../config";
 import "./Auth.css";
-
+console.log("API URL:", API);
 function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
