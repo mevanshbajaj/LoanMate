09d62837f4c8fd50460e6d89fd75f21d7b7f17a0
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-02-26 15:04:08 +0530

    database

--- loanmate-backend/config/db.js
+++ loanmate-backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb://127.0.0.1:27017/loanmate"
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
