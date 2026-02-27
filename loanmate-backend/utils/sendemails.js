2f57d5b9221df3c1596672f7c43f21fad85a5388
Vansh Bajaj <mevanshbajaj@gmail.com>
2026-02-27 11:45:03 +0530

    Switch SMTP to port 587

--- loanmate-backend/utils/sendemails.js
+++ loanmate-backend/utils/sendemails.js
@@ -1,18 +1,25 @@
 const nodemailer = require("nodemailer");
 
-exports.sendEmail = async (to, subject, text) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
 
-  await transporter.sendMail({
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
-  });
+exports.sendEmail = async (to, subject, text) => {
+  try {
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text
+    });
+    console.log("Email sent:", info.response);
+  } catch (err) {
+    console.error("Email error:", err);
+  }
 };
